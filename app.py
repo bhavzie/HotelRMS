ce64159192c07bd2eb3dbcf9ff2219188f43881a
bhavzie <koolbhavya.epic@gmail.com>
2020-07-14 18:56:57 +0530

    bugs

--- app.py
+++ app.py
@@ -5964,5 +5964,5 @@ def resubmitRequest():
     return ('', 204)
     
 if __name__ == "__main__":
-    app.run(debug = True, threaded = True, host='0.0.0.0')
+    app.run(debug = True, threaded = True)
 
