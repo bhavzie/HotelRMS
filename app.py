3df509ed700b56c676fc45364a831f3ed7b79f7d
bhavzie <koolbhavya.epic@gmail.com>
2020-08-02 09:49:50 +0530

    0000

--- app.py
+++ app.py
@@ -6548,5 +6548,5 @@ def addCustomerSubmit():
         return redirect(url_for('customerT'))
 
 if __name__ == "__main__":
-    app.run(debug = True, threaded = True)
+    app.run(debug = True, threaded = True, host = '0.0.0.0')
 
